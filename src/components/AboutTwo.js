29ec0382cdd167c993efcc0548765f68543995d6
Juan Daniel Sanchez <32243398+DseeHappy@users.noreply.github.com>
2021-04-17 16:35:13 -0600

    image and text

--- src/components/AboutTwo.js
+++ src/components/AboutTwo.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import CountUp from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
-import about1 from "../assets/images/about-1-1.jpg";
+import about1 from "../assets/images/high-view/img/High-Views-Seamless-Gutters-Gutter-Close-Up-529x529.png";
+import { Link } from 'gatsby';
 export default class AboutTwo extends Component {
   constructor() {
     super();
     this.state = {
-      startCounter: false
+      startCounter: false,
     };
   }
 
@@ -25,40 +26,23 @@ export default class AboutTwo extends Component {
               <div className="about-two__content">
                 <div className="block-title text-left">
                   <h2 className="block-title__title">
-                    Welcome to online <br />
-                    learning center
+                    Why Choose High View's Seamless Gutters?
                   </h2>
                 </div>
                 <p className="about-two__text">
-                  There are many variations of passages of lorem ipsum available
-                  but the majority have suffered alteration in some form by
-                  injected humour or randomised words which don't look.
+                  No matter the weather, High View is here ready for any and all
+                  your repair needs.
                 </p>
-                <div className="about-two__single-wrap">
-                  <div className="about-two__single">
-                    <div className="about-two__single-icon">
-                      <i className="kipso-icon-professor"></i>
-                    </div>
-                    <div className="about-two__single-content">
-                      <p className="about-two__single-text">
-                        Start learning from our experts
-                      </p>
-                    </div>
-                  </div>
-                  <div className="about-two__single">
-                    <div className="about-two__single-icon">
-                      <i className="kipso-icon-knowledge"></i>
-                    </div>
-                    <div className="about-two__single-content">
-                      <p className="about-two__single-text">
-                        Enhance your skills with us now
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <a href="#none" className="thm-btn">
-                  Learn More
-                </a>
+                <hr />
+                <p className="about-two__text">
+                  We want to make sure you’re 100% satisfied, and we go the
+                  extra mile to make your experience as enjoyable and
+                  stress-free as possible.
+                </p>
+<br/>
+                <Link href="#none" className="thm-btn">
+                  Check out our Services!
+                </Link>
               </div>
             </div>
             <div className="col-xl-6 d-flex justify-content-xl-end justify-content-sm-center">
@@ -67,14 +51,14 @@ export default class AboutTwo extends Component {
                 <img src={about1} alt="" />
                 <div className="about-two__count">
                   <div className="about-two__count-text">
-                    Trusted by
+                    Installed over
                     <span className="counter">
                       <VisibilitySensor
                         onChange={this.onVisibilityChange}
                         offset={{ top: 10 }}
                         delayedCall
                       >
-                        <CountUp end={this.state.startCounter ? 4890 : 0} />
+                        <CountUp end={this.state.startCounter ? 2548 : 0} />
                       </VisibilitySensor>
                     </span>
                   </div>
