a0f0f98781619081461b300499340493e0f91b14
Juan Daniel Sanchez <32243398+DseeHappy@users.noreply.github.com>
2021-04-17 20:54:03 -0600

    nav

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from "../components/core/Layout";
-import NavOne from "../components/NavOne";
+import NavOne from "../components/core/NavThree";
 import PageHeader from "../components/PageHeader";
 import Footer from "../components/core/Footer";
 import Gallery from "../components/Gallery";
