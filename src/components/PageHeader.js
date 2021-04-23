7fd5007269acd0b082ad2b8ee26315748ba22b3b
Juan Daniel Sanchez <32243398+DseeHappy@users.noreply.github.com>
2021-04-23 16:21:50 -0600

    updates to images loading and styling

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,21 +1,54 @@
 import React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const PageHeader = (props) => {
   return (
-    <section className="inner-banner">
-      <div className="container">
-        <ul className="list-unstyled thm-breadcrumb">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li className="active">
-            <a href="#none">{props.title}</a>
-          </li>
-        </ul>
-        <h2 className="inner-banner__title">{props.title}</h2>
+    <div className="inner-banner" style={{ display: "grid"  }}>
+      {/* You can use a GatsbyImage component if the image is dynamic */}
+      <StaticImage
+        style={{
+          gridArea: "1/1",
+          // You can set a maximum height for the image, if you wish.
+          minHeight: 600,
+        }}
+        layout="fullWidth"
+        // You can optionally force an aspect ratio for the generated image
+        aspectRatio={3 / 1}
+        // This is a presentational image, so the alt should be an empty string
+        alt=""
+        // Assisi, Perúgia, Itália by Bernardo Ferrari, via Unsplash
+        src={
+          "../assets/images/high-view/img/High-Views-Seamless-Gutters-LeafBlaster-Gutter-Guard-1024x368.png"
+        }
+        formats={["auto", "webp", "avif"]}
+      />
+      <div
+        style={{
+          // By using the same grid area for both, they are stacked on top of each other
+          gridArea: "1/1",
+          position: "relative",
+          // This centers the other elements inside the hero component
+          placeItems: "center",
+          display: "grid",
+          backgroundColor:"rgba(1, 34, 55, 0.6)"
+
+        }}
+      >
+        {/* Any content here will be centered in the component */}
+        <div className="container">
+          <ul className="list-unstyled thm-breadcrumb">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li className="active">
+              <a href="#none">{props.title}</a>
+            </li>
+          </ul>
+          <h2 className="inner-banner__title">{props.title}</h2>
+        </div>{" "}
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/components/SliderOne.js
+++ src/components/SliderOne.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
-import Swiper from "react-id-swiper";
+import { StaticImage } from "gatsby-plugin-image";
+
 import "swiper/css/swiper.css";
-import slidestrech from "../assets/images/slider-1-scratch.png";
-import person1 from "../assets/images/slider-1-person-1.png";
-import person2 from "../assets/images/slider-1-person-2.png";
-const SliderOne = () => {
+
+const SliderOne = (props) => {
   const [swiper, setSwiper] = useState(null);
 
   const goNext = () => {
@@ -21,13 +20,39 @@ const SliderOne = () => {
   };
 
   return (
-    <div className="banner-wrapper">
-      <section className="banner-one banner-carousel__one no-dots">
-        <Swiper getSwiper={setSwiper}>
+    <>
+      <div className="banner-wrapper" style={{ display: "grid" }}>
+        {/* You can use a GatsbyImage component if the image is dynamic */}
+        <StaticImage
+          style={{
+            gridArea: "1/1",
+            // You can set a maximum height for the image, if you wish.
+            minHeight: 1350,
+          }}
+          loading="eager"
+          layout="fullWidth"
+          aspectRatio={2 / 1}
+          // You can optionally force an aspect ratio for the generated image
+          // This is a presentational image, so the alt should be an empty string
+          alt="High View's Seamless Gutters Denver CO Quality Seamless Gutters "
+          src={"../assets/images/high-view/img/house4k.png"}
+          formats={["auto", "webp", "avif"]}
+        />
+        <div
+          style={{
+            // By using the same grid area for both, they are stacked on top of each other
+            gridArea: "1/1",
+            position: "relative",
+            // This centers the other elements inside the hero component
+            placeItems: "center",
+            display: "grid",
+            backgroundColor:"rgba(1, 34, 55, 0.9)"
+
+          }}
+        >
+          {/* Any content here will be centered in the component */}
           <div className="banner-one__slide banner-one__slide-one">
             <div className="container">
-             
-              <img src={person1} className="banner-one__person" alt="" />
               <div className="row no-gutters">
                 <div className="col-xl-12">
                   <h3 className="banner-one__title banner-one__light-color">
@@ -37,7 +62,7 @@ const SliderOne = () => {
                     Everything installed by us is ready to stand the weather
                     here in Colorado
                   </p>
-                  
+
                   <Link to="/gallery" className="thm-btn banner-one__btn">
                     Check out our Previous Work!
                   </Link>
@@ -45,47 +70,26 @@ const SliderOne = () => {
               </div>
             </div>
           </div>
-          <div className="banner-one__slide banner-one__slide-two">
-            <div className="container">
-             
-              <img src={person2} className="banner-one__person" alt="" />
-              <div className="row no-gutters">
-                <div className="col-xl-12">
-                  <h3 className="banner-one__title banner-one__light-color">
-                    Let our Gutter Pro's <br /> take care of your gutters
-                  </h3>
-                  <p className="banner-one__tag-line">
-                    Everything installed by us is ready to stand the weather
-                    here in Colorado
-                  </p>
-                  <Link to="/gallery" className="thm-btn banner-one__btn">
-                    Check out our Previous Work!
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Swiper>
-      </section>
-  
-      <div className="banner-one__cta">
-        <div className="banner-one__cta-icon">
-          <i class="fas fa-donate"></i>
         </div>
-        <div className="banner-one__cta-title">
-          <h3 className="banner-one__cta-text">
+        <div className="banner-one__cta">
+          <div className="banner-one__cta-icon">
+            <i class="fas fa-donate"></i>
+          </div>
+          <div className="banner-one__cta-title">
+            <h3 className="banner-one__cta-text">
+              <Link to="/financing">
+                Financing available up to $65,000, Find out more
+              </Link>
+            </h3>
+          </div>
+          <div className="banner-one__cta-link">
             <Link to="/financing">
-              Financing available up to $65,000, Find out more
+              <i className="kipso-icon-right-arrow"></i>
             </Link>
-          </h3>
-        </div>
-        <div className="banner-one__cta-link">
-          <Link to="/financing">
-            <i className="kipso-icon-right-arrow"></i>
-          </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default SliderOne;
