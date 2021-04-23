0971ea617c9e8604583ed89d27af129e72fedfc6
Juan Daniel Sanchez <32243398+DseeHappy@users.noreply.github.com>
2021-04-23 17:02:10 -0600

    cleanup for image loading

--- src/components/CallToActionOne.js
+++ src/components/CallToActionOne.js
@@ -1,29 +1,58 @@
 import React from "react";
 import ctabg1 from "../assets/images/high-view/img/largeroofA.png";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const CallToActionOne = () => {
   return (
-    <section
-      className="cta-one cta-one__home-one"
-      style={{ backgroundImage: `url(${ctabg1})` }}
-    >
-      <div className="container">
-        <h2 className="cta-one__title">At High View's Seamless Gutters</h2>
-        <h2 className="cta-one__title">
-          We pride ourselves on masterful installation.
-        </h2>
-        <div className="cta-one__btn-block">
-          <a
-            href="https://book.housecallpro.com/book/High-view-seamless-gutters/c03bbfa024d84f109c8e4e6af991bec4"
-            target="__blank"
-            className="thm-btn cta-one__btn"
-          >
-            Schedule your Gutter Services Online!
-          </a>
-        </div>
+    <div className="cta-one cta-one__home-one" style={{ display: "grid" }}>
+      {/* You can use a GatsbyImage component if the image is dynamic */}
+      <StaticImage
+        style={{
+          gridArea: "1/1",
+          // You can set a maximum height for the image, if you wish.
+          // maxHeight: 600,
+        }}
+        layout="fullWidth"
+        // You can optionally force an aspect ratio for the generated image
+        aspectRatio={2 / 1}
+        // This is a presentational image, so the alt should be an empty string
+        alt=""
+        // Assisi, Perúgia, Itália by Bernardo Ferrari, via Unsplash
+        src={"../assets/images/high-view/img/largeroofA.png"}
+        formats={["auto", "webp", "avif"]}
+      />
+      <div
+        style={{
+          // By using the same grid area for both, they are stacked on top of each other
+          gridArea: "1/1",
+          position: "relative",
+          // This centers the other elements inside the hero component
+          placeItems: "center",
+          display: "grid",
+          backgroundColor: "rgba(241,97,1,.8)",
+        }}
+      >
+        {/* Any content here will be centered in the component */}
+        <div className="container">
+          <h2 className="cta-one__title">At High View's Seamless Gutters</h2>
+          <br />
+          <h2 className="cta-one__title">
+            We pride ourselves on masterful installation.
+          </h2>
+          <hr />
+          <div className="cta-one__btn-block">
+            <a
+              href="https://book.housecallpro.com/book/High-view-seamless-gutters/c03bbfa024d84f109c8e4e6af991bec4"
+              target="__blank"
+              className="thm-btn cta-one__btn"
+            >
+              Schedule your Gutter Services Online!
+            </a>
+          </div>
+        </div>{" "}
       </div>
-    </section>
+    </div>
   );
 };
 
